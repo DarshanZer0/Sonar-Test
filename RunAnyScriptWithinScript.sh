9056ef29d828fe069029b21b350c2bf10e3a9202
DarshanZer0 <darshan.c@exilant.com>
2016-08-02 15:43:00 +0530

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "commiting all the files to git"""""""
    This reverts commit 6b58d9d6827597c63445896e342a85766109e560.

--- RunAnyScriptWithinScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#This script is to run another script and create log files in a specific directory
-#Author : Darshan C
-#Date 	: 27-July-2016
-
-DATE=`date +%B_%d_%Y_%H_%M_%S`
-`./len.sh > ~/Desktop/DC-Tests/ResultLog_$DATE`
--- test1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-NAME="Darshan C"
-echo "DC is here"
-echo "DC stands for $NAME"
-echo "My age is $1"
